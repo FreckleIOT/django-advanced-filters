be66721a3fe568f6da7e32937a9313f62070bc32
crodriguez <crodriguez@octobot.io>
2019-05-30 09:44:28 -0300

    :feat Show specific filter types for each type of attribute

--- advanced_filters/urls.py
+++ advanced_filters/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from advanced_filters.views import GetFieldChoices
+from advanced_filters.views import GetFieldChoices, GetOperatorChoices
 
 urlpatterns = [
     url(r'^field_choices/(?P<model>.+)/(?P<field_name>.+)/?',
@@ -11,4 +11,12 @@ urlpatterns = [
     url(r'^field_choices/$',
         GetFieldChoices.as_view(),
         name='afilters_get_field_choices'),
+
+    url(r'^operator_choices/(?P<model>.+)/(?P<field_name>.+)/?',
+        GetOperatorChoices.as_view(),
+        name='afilters_get_operator_choices'),
+
+    url(r'^operator_choices/$',
+        GetOperatorChoices.as_view(),
+        name='afilters_get_operator_choices'),
 ]
